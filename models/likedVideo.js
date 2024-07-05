5c7ef1af1bb4885899746830db30f0e0209757be
venture1606 <venture1606@gmail.com>
2024-07-05 20:12:01 +0530

    completing the video straming platform

--- models/likedVideo.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const likedVideoSchema= mongoose.Schema({
-    videoId: {type:String,require:true},
-    Viewer: {type:String,require:true},
-    LikedOn:{type:Date,default: Date.now}
-})
-
-export default mongoose.model('LikedVideo',likedVideoSchema)
